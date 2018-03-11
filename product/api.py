079c320bfeb998cb078067bb5c7abba8b5f49cc2
Sebastian Lin <jersheng.lin@duke.edu>
2018-03-10 17:01:10 -0800

    first commit to docker_comose

---
+++ product/api.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Product(Resource):
+    def get(self):
+        return {
+            "product": ["Ice cream", "Chocolate", "Fruit"]
+        }
+
+api.add_resource(Product, "/")
+
+def main():
+    app.run(host="0.0.0.0", port=80, debug=True)
+
+if __name__ == "__main__":
+    main()
